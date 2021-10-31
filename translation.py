bcf13ec5ad19c3c84a77c947b98017b1d769c40e
Jikku <66976170+jikkubot@users.noreply.github.com>
2021-10-31 21:25:34 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,7 @@ class Translation(object):
 😎I can upload to telegram as File or Video or Audio(if supported) format.
 
 /help for more details</b>"""
-    FORMAT_SELECTION = "<b>🌀Select the desired format: <a href='{}'>File size might be approximate</a> \n\n🖌️If you want to set custom thumbnail.\n🎯Send a photo before or quickly after tapping on any of the below buttons.\n\n🍿You can use /delthumbnail to delete the auto-generated thumbnail.</b>"
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     DOWNLOAD_START = "<b>📥Downloading Now...</b>"
